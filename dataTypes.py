ea9504b018ff13e97f5527c9e90d171be9bb6a03
aariandhanani <47797960+aariandhanani@users.noreply.github.com>
2020-09-14 10:27:56 -0500

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,26 @@
+#Aarian Dhanani
+#9/12/20
+#Basic Program with different Data Types
+
+a = 1
+b = 1.5
+c = True
+d = "hello"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+e = a + b
+print(e)
+e = a - b
+print(e)
+e = a * b
+print(e)
+e = a / b
+print(e)
+
+e = a % b
+print(e)
+print("Modulus gets the remainder of the two numbers after they are divided.")
---
+++ project1.py
@@ -0,0 +1,19 @@
+counter = 100
+miles   = 1000.0
+name    = "John"
+
+print (counter)
+print (miles)
+print (name)
+
+a = 1
+b = 2
+
+a = b = 5
+
+print (a)
+print(b)
+
+str = "Hello"
+
+print (str)
