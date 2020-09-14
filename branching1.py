9ab013000342835f3c64a1ecee6ec483f2ffb6ce
aariandhanani <aariandhanani@gmail.com>
2020-09-14 11:40:43 -0500

    new files

---
+++ branching1.py
@@ -0,0 +1,13 @@
+#Aarian Dhanani
+
+a = 7
+b = 13
+c = 14
+d = 13
+
+if a % 2 == 0:
+    print(b+c)
+elif b % 2 == 0:
+    print(c-d)
+else:
+    print(d*c/a)
---
+++ looping.py
@@ -0,0 +1,12 @@
+#Aarian Dhanani
+
+print("1 2 3 4 5 6 7 8 9")
+
+print("1\n" + "2\n" + "3\n" + "4\n")
+
+print("1\n")
+print("2 2\n")
+print("3 3 3\n")
+
+for x in range(9):
+    print(x * 2)
