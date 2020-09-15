9afd1e46aa0223c01373cbe5bd3f1c050f824e8e
aariandhanani <aariandhanani@gmail.com>
2020-09-15 18:54:43 -0500

    Update forLoops.py

--- forLoops.py
+++ forLoops.py
@@ -1,16 +1,18 @@
 #Aarian Dhanani
 #9/15/20
 
-import sys
 
-print(sys.version)
+for x in range(9):
+    print(x + 1, end = " ")
+
+print("\n")
+
 for x in range(9):
     print(x + 1)
 
-for x in range(9):
-    print(x + 1)
+print("\n")
 
 for x in range(9):
     for y in range(x + 1):
-        testing = x + 1
-        # print(testing, end = ' ', flush = true);
+        print(x + 1, end = " ")
+    print("\n")
