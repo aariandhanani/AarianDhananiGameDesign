8f85aa8377f5b23b1c76ddb95c116b8c8ff7da91
aariandhanani <aariandhanani@gmail.com>
2020-09-15 17:58:06 -0500

    Create forLoops.py

---
+++ forLoops.py
@@ -0,0 +1,16 @@
+#Aarian Dhanani
+#9/15/20
+
+import sys
+
+print(sys.version)
+for x in range(9):
+    print(x + 1)
+
+for x in range(9):
+    print(x + 1)
+
+for x in range(9):
+    for y in range(x + 1):
+        testing = x + 1
+        # print(testing, end = ' ', flush = true);
