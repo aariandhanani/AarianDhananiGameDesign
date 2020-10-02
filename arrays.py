240613fcdecbf18da2bbb733b1ca2d8d35762b73
aariandhanani <aariandhanani@gmail.com>
2020-10-02 11:01:08 -0500

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,3 +1,21 @@
 #Aarian Dhanani
+#array printing
 
 myList = [2, 4, 5, 7, 8, 9]
+myFruit = ["Apple", "Banana", "Orange"]
+
+print(len(myFruit))
+# print(myList[1])
+# print(myList[-2])
+print(myList[1:4])
+
+if 4 in myList:
+    print("yes")
+
+if "berries" in myFruit:
+    print("I love banana")
+else:
+    print("buy some berries")
+
+# for x in myList:
+#     print(x)
