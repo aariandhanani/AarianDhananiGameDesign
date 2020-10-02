9cf3ec199238cf3266e70472cc873024a0cfb1de
aariandhanani <aariandhanani@gmail.com>
2020-10-02 10:34:05 -0500

    changes

--- challengeQuestionDueOctober2nd.py
+++ challengeQuestionDueOctober2nd.py
@@ -3,25 +3,36 @@
 
 words = "Here are the instructions to install Drivers\n1. After the download is completed go to where you saved the folder.\n(By default everything you download from the Internet is saved to the Downloads folder)\n2. Right click on the folder and choose ''Extract All'' and then choose ''Extract'' again.\n3. Once all the contents have been extracted you may delete/disregard the folder with the zip icon.\n4. Next, open and Run the SETUP file. (In most cases it is a setup.exe file OR one listed below:\n*setup application\n*Asussetup\n*pnpinstal64\n*pnputil\n*Igxpin\n5. Please choose to 'repair' or 'update' the existing installation (driver) IF any one of those options do appear during the set up."
 
+#Find how many times the word Drivers is used
 #Source: https://www.programiz.com/python-programming/methods/string/count#:~:text=The%20string%20count()%20method,substring%20is%20present%20in%20it.
 numberOfDrivers = words.count("Drivers")
 print(numberOfDrivers)
 
+#How long in your String
 print(len(words))
 
+#Replace Extract with EXTRACT
 words = words.replace("Extract", "EXTRACT")
 
+#Replace setup or Setup with SETUP
 words = words.replace("setup", "SETUP")
 words = words.replace("Setup", "SETUP")
 
+#Find ‘4’
 indexOf4 = words.find("4")
 print("The index of 4 is: ", end = "")
 print(indexOf4)
 
+#The first \n
 indexOfFirstEnter = words.find("\n")
 print(indexOfFirstEnter)
 
-print()
+
+# Find the 1 and print until you find the end of the statement
+# Find the 2 and print until you find the end of the statement
+# Find the 3 and print until you find the end of the statement
+# Find the 4 and print until you find the end of the statement
+# Find the 5 and print until you find the end of the statement
 
 indexOf1 = words.find("1")
 indexOf2 = words.find("2")
