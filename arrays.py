0b5b4eea93b4df06c0c3f0085213f7acd2994ed1
aariandhanani <aariandhanani@gmail.com>
2020-10-02 12:15:59 -0500

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -2,6 +2,7 @@
 #array printing
 
 myList = [2, 4, 5, 7, 8, 9]
+yourList = [98, 87, 65]
 myFruit = ["Apple", "Banana", "Orange"]
 
 # myFruit.remove("Apple")
@@ -26,6 +27,17 @@ if "berries" in myFruit:
     print("I love banana")
 else:
     print("buy some berries")
+
+ourList = myList + yourList
+print(ourList)
+
+myList.extend(myFruit)
+
+print(myList)
+
+
+myFruit.sort()
+print(myFruit)
 
 # for x in myList:
 #     print(x)
