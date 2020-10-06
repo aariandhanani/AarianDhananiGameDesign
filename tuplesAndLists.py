b94e4a62f30a57f94a6ea6479e2daf9def55c98f
aariandhanani <aariandhanani@gmail.com>
2020-10-06 11:23:35 -0500

    Update tuplesAndLists.py

--- tuplesAndLists.py
+++ tuplesAndLists.py
@@ -5,7 +5,7 @@
 from copy import deepcopy
 
 # 1. Write a Python program to create a tuple.
-tuple = ("hi", "hello")
+tuple1 = ("hi", "hello")
 
 # 2. Write a Python program to create a tuple with different data types.
 tupleWithMultipleDataTypes = ("hi", 1, False)
@@ -45,12 +45,12 @@ print(tuple2)
 
 # 9. Write a Python program to find the repeated items of a tuple.
 tupleRepeat = (1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10)
-list = []
+list1 = []
 for x in range((len(tupleRepeat)) - 1):
     temporary = tupleRepeat.count(tupleRepeat[x])
-    if temporary > 1 and list.count(tupleRepeat[x]) == 0:
+    if temporary > 1 and list1.count(tupleRepeat[x]) == 0:
         print(tupleRepeat[x])
-        list.append(tupleRepeat[x])
+        list1.append(tupleRepeat[x])
 
 # 10. Write a Python program to check whether an element exists within a tuple.
 tupleElement2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
@@ -61,23 +61,31 @@ else:
     print("No")
 
 # 11. Write a Python program to convert a list to a tuple.
-# listNew = ["hi", "ok", "hello"]
-# listNew = tuple(listNew)
+listNew = ["hi", "ok", "hello"]
+tupleNew = tuple(listNew)
 
 # 12. Write a Python program to remove an item from a tuple.
-
+# It is not possible to remove an item from a tuple.
 
 # 13. Write a Python program to slice a tuple.
-
+tupleSlice = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(tupleSlice[3:8])
 
 # 14. Write a Python program to find the index of an item of a tuple.
+tupleFind = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(tupleFind.index(5))
 
 
 # 15. Write a Python program to find the length of a tuple.
-
+tupleLength = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(len(tupleLength))
 
 # 16. Write a Python program to convert a tuple to a dictionary.
+# ?
+
 # 17. Write a Python program to unzip a list of tuples into individual lists.
+# ?
+
 # 18. Write a Python program to reverse a tuple.
 # 19. Write a Python program to convert a list of tuples into a dictionary.
 # 20. Write a Python program to print a tuple with string formatting.Sample tuple : (100, 200, 300)Output :This is a tuple (100, 200, 300)
@@ -93,3 +101,8 @@ else:
 # https://www.geeksforgeeks.org/python-program-to-convert-a-tuple-to-a-string/
 # https://www.w3resource.com/python-exercises/tuple/python-tuple-exercise-8.php
 # https://stackoverflow.com/questions/12836128/convert-list-to-tuple-in-python
+# https://stackoverflow.com/questions/12836128/convert-list-to-tuple-in-python
+# https://www.geeksforgeeks.org/python-convert-a-list-into-a-tuple/
+# https://www.pythoncentral.io/how-to-slice-listsarrays-and-tuples-in-python/
+# https://thispointer.com/python-how-to-find-an-element-in-tuple-by-value/
+# https://stackoverflow.com/questions/31087111/typeerror-list-object-is-not-callable-in-python/55115952
