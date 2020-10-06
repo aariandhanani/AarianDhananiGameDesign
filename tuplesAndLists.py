7f94b547c6689a4064083f1f498f5943863707a1
aariandhanani <aariandhanani@gmail.com>
2020-10-05 22:35:58 -0500

    Create tuplesAndLists.py

---
+++ tuplesAndLists.py
@@ -0,0 +1,11 @@
+# Aarian Dhanani
+# 10/5/20
+# Tuples and Lists Assignment
+
+# 1. Write a Python program to create a tuple.
+
+
+
+
+# Sources:
+# https://www.programiz.com/python-programming/tuple
