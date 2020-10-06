76d01144414f53799584caacfe86637a3ef3ceb9
aariandhanani <aariandhanani@gmail.com>
2020-10-06 10:19:44 -0500

    Update tuplesAndLists.py

--- tuplesAndLists.py
+++ tuplesAndLists.py
@@ -3,7 +3,31 @@
 # Tuples and Lists Assignment
 
 # 1. Write a Python program to create a tuple.
+tuple = ("hi", "hello")
 
+# 2. Write a Python program to create a tuple with different data types
+tupleWithMultipleDataTypes = ("hi", 1, False)
+
+# 3. Write a Python program to create a tuple with numbers and print one item.
+
+
+# 4. Write a Python program to unpack a tuple in several variables.
+# 5. Write a Python program to add an item in a tuple.
+# 6. Write a Python program to convert a tuple to a string.
+# 7. Write a Python program to get the 4th element and 4th element from the last of a tuple
+# 8. Write a Python program to create the colon of a tuple
+# 9. Write a Python program to find the repeated items of a tuple.
+# 10. Write a Python program to check whether an element exists within a tuple.
+# 11. Write a Python program to convert a list to a tuple.
+# 12. Write a Python program to remove an item from a tuple.
+# 13. Write a Python program to slice a tuple.
+# 14. Write a Python program to find the index of an item of a tuple.
+# 15. Write a Python program to find the length of a tuple.
+# 16. Write a Python program to convert a tuple to a dictionary.
+# 17. Write a Python program to unzip a list of tuples into individual lists.
+# 18. Write a Python program to reverse a tuple.
+# 19. Write a Python program to convert a list of tuples into a dictionary.
+# 20. Write a Python program to print a tuple with string formatting.Sample tuple : (100, 200, 300)Output :This is a tuple (100, 200, 300)
 
 
 
