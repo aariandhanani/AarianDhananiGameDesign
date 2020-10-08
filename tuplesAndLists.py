10a35e46e481b03e08adfe543ca8fe394c80ba59
aariandhanani <aariandhanani@gmail.com>
2020-10-08 10:45:47 -0500

    Update tuplesAndLists.py

--- tuplesAndLists.py
+++ tuplesAndLists.py
@@ -103,19 +103,30 @@ print(tupleReverse)
 # Skip
 
 # 20. Write a Python program to print a tuple with string formatting.Sample tuple : (100, 200, 300)Output :This is a tuple (100, 200, 300)
-
+thisIsATuple = (100, 200, 300)
+print("This is a tuple ", end = "")
+print(thisIsATuple)
 
 # 21. Write a Python program to replace last value of tuples in a list.Sample list: [(10, 20, 40), (40, 50, 60), (70, 80, 90)]Expected Output: [(10, 20, 100), (40, 50, 100), (70, 80, 100)]
+replace1 = [(1, 2), (3, 4), (5, 6)]
 
+# Confused
 
 # 22. Write a Python program to remove an empty tuple(s) from a list of tuples.Sample data: [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]Expected output: [('',), ('a', 'b'), ('a', 'b', 'c'), 'd']
 
 
 # 23. Write a Python program to sort a tuple by its float element.Sample data: [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]Expected Output: [('item3', '24.5'), ('item2', '15.10'), ('item1', '12.20')]
-
+# Skip
 
 # 24. Write a Python program to count the elements in a list until an element is a tuple.
+elements1 = [1, 2, (3, 4), 5, 6]
+counter = 0
+for number in elements1:
+    if isinstance(number, tuple):
+        break
+    counter = counter + 1
 
+print(counter)
 
 
 # Sources:
