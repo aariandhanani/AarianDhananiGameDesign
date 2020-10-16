f0c3759a9cf41dc6120b448eda30fdf6789290b8
aariandhanani <aariandhanani@gmail.com>
2020-10-16 11:39:38 -0500

    Update game1.py

--- game1.py
+++ game1.py
@@ -8,4 +8,30 @@ import time
 name = input("What is your name? ")
 print("Hello ", name)
 
-# time.sleep(3)
+gameWords = ['python', 'javascript', 'java', 'PHP', 'hardware', 'software', 'laptop', 'keyboard']
+
+#use the choice method of the random function to pick a word
+
+answer = input("Do you want to guess a word?")
+
+while answer == "yes":
+    word = random.choice(gameWords)
+    guesses = ""
+    turns = 5
+    while turns > 0:
+        for char in word:
+            if char in guesses:
+                print(char, end = "")
+            else:
+                print("_", end = "")
+        print()
+        guess = input("Guess a letter or word")
+        guesses += guess
+        turns-= 1
+    
+    answer= input("Do you want to play again?")
+
+word = random.choice(gameWords)
+print(word)
+
+time.sleep(3)
