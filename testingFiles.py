f943aa0a4ec29ddf2f11d28e13005bbbca5a7a4c
aariandhanani <aariandhanani@gmail.com>
2020-10-28 10:50:35 -0500

    changes

--- testingFiles.py
+++ testingFiles.py
@@ -1,2 +1,16 @@
 # Aarian Dhanani
 # 10/28/20
+#
+# Learning about files
+#
+# open/create
+# close
+# write "w"
+# append "a"
+# read "r"
+
+myFile = open("newFile.txt", "w") #Opens a file if it exists and if it doesn't exist it will create a file
+
+myFile.write("Hello")
+
+myFile.close()
