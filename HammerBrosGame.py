e1468ea16e62bfe33b59c1d0229a90842fbed4d5
aariandhanani <aariandhanani@gmail.com>
2020-11-12 12:48:12 -0600

    Changes

--- HammerBrosGame.py
+++ HammerBrosGame.py
@@ -8,8 +8,9 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT)) # this is a tuple
 
 # screen.fill((R,G,B))
 
+walkLeft = [pygame.image.load("WalkLeft/1.png"), pygame.image.load("WalkLeft/1.png"), pygame.image.load("WalkLeft/2.png"), pygame.image.load("WalkLeft/2.png"), pygame.image.load("WalkLeft/3.png"), pygame.image.load("WalkLeft/3.png"), pygame.image.load("WalkLeft/4.png"), pygame.image.load("WalkLeft/4.png")]
 background = pygame.image.load("brickWall copy.png")
-image1 = pygame.image.load("1.png")
+# image1 = pygame.image.load("1.png")
 screen.fill((113, 96, 255))
 pygame.display.set_caption("Game")
 
@@ -19,15 +20,17 @@ run = True
 Jump = False
 high = 10
 
-x = 200
-y = 200
+x = 400
+y = 400
 w = 50
 h = 100
 r = 50
 
+leftImage = 0
+
 def redrawWindow():
     screen.blit(background, (0,0))
-    screen.blit(image1, (x,y))
+    screen.blit(walkLeft[leftImage], (x,y))
     pygame.display.update()
 
 
@@ -44,7 +47,7 @@ while run:
 
     keyboardKey = pygame.key.get_pressed()
 
-    speed = 10
+    speed = 5
 
     #if keyboardKey[pygame.K_UP]:
         #y = y - speed
@@ -53,6 +56,10 @@ while run:
         #y = y + speed
          
     if keyboardKey[pygame.K_LEFT] and x > speed:
+        if leftImage != 3:
+            leftImage += 1
+        else:
+            leftImage = 0
         x = x - speed
         r = r - speed
 
@@ -61,16 +68,16 @@ while run:
         r = r + speed
 
     if not(Jump):
-        if keyboardKey[pygame.K_UP] and y > speed:
-            y -= speed
-        if keyboardKey[pygame.K_DOWN] and y < HEIGHT - h - speed:
-            y += speed
+        #if keyboardKey[pygame.K_UP] and y > speed:
+            #y -= speed
+        #if keyboardKey[pygame.K_DOWN] and y < HEIGHT - h - speed:
+            #y += speed
         if keyboardKey[pygame.K_SPACE]:
             Jump = True
     else:
         if high >= -10:
             y -= ((high * abs(high)) / 2)
-            y -= 1
+            high -= 1
         else:
             high = 10
             Jump = False
