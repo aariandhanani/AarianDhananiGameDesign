951f762c7a0ef4c15eb559e30b8d8908bfcadd21
aariandhanani <aariandhanani@gmail.com>
2020-11-16 13:51:17 -0600

    Changes

--- HammerBrosGame.py
+++ HammerBrosGame.py
@@ -1,9 +1,16 @@
+# Note: Instead of using clock = pygame.time.clock(), I just have the same image next to each other in the list. I believe that this solves the frame issue.
+
+
 import pygame
 
 pygame.init()
 
-WIDTH = 960
-HEIGHT = 640
+# WIDTH = 960
+# HEIGHT = 640
+
+WIDTH = 1000
+HEIGHT = 600
+
 screen = pygame.display.set_mode((WIDTH,HEIGHT)) # this is a tuple
 
 # screen.fill((R,G,B))
@@ -11,7 +18,9 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT)) # this is a tuple
 walkLeft = [pygame.image.load("WalkLeft/1.png"), pygame.image.load("WalkLeft/1.png"), pygame.image.load("WalkLeft/2.png"), pygame.image.load("WalkLeft/2.png"), pygame.image.load("WalkLeft/3.png"), pygame.image.load("WalkLeft/3.png"), pygame.image.load("WalkLeft/4.png"), pygame.image.load("WalkLeft/4.png")]
 walkRight = [pygame.image.load("WalkRight/1.png"), pygame.image.load("WalkRight/1.png"), pygame.image.load("WalkRight/2.png"), pygame.image.load("WalkRight/2.png"), pygame.image.load("WalkRight/3.png"), pygame.image.load("WalkRight/3.png"), pygame.image.load("WalkRight/4.png"), pygame.image.load("WalkRight/4.png")]
 
-background = pygame.image.load("brickWall copy.png")
+#background = pygame.image.load("brickWall copy.png")
+background = pygame.image.load("modifiedMarioNew.png")
+
 # image1 = pygame.image.load("1.png")
 screen.fill((113, 96, 255))
 pygame.display.set_caption("Game")
@@ -22,8 +31,10 @@ run = True
 Jump = False
 high = 10
 
-x = 400
-y = 400
+backgroundX = 0
+
+x = 100
+y = 392
 w = 50
 h = 100
 r = 50
@@ -33,14 +44,14 @@ rightImage = 0
 leftOrRight = 1
 
 # To control the frames
-clock = pygame.time.clock()
+#clock = pygame.time.clock()
 
-# control left and right movements
+# control left and right movements. Instead of using this, I use leftOrRight
 left = False
 right = False
 
 def redrawWindow():
-    screen.blit(background, (0,0))
+    screen.blit(background, (backgroundX,0))
     if leftOrRight == 0:  
         screen.blit(walkLeft[leftImage], (x,y))
     else:
@@ -78,7 +89,7 @@ while run:
         x = x - speed
         r = r - speed
 
-    if keyboardKey[pygame.K_RIGHT] and x < WIDTH - w - speed:
+    if keyboardKey[pygame.K_RIGHT] and x < WIDTH:
         leftOrRight = 1
         if rightImage != 3:
             rightImage += 1
@@ -86,6 +97,20 @@ while run:
             rightImage = 0
         x = x + speed
         r = r + speed
+
+    #if keyboardKey[pygame.K_RIGHT] and x < WIDTH - w - speed:
+        #leftOrRight = 1
+        #if rightImage != 3:
+            #rightImage += 1
+        #else:
+            #rightImage = 0
+        #x = x + speed
+        #r = r + speed
+    
+
+    if x > WIDTH:
+        x = 400
+        backgroundX = 1000
 
     if not(Jump):
         #if keyboardKey[pygame.K_UP] and y > speed:
